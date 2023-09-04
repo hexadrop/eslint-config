715e4e16a408231f5ebeea0b771752b69fa60c63
MarioMH8 <mhidalgomario@gmail.com>
2023-09-04 03:14:04 +0200

    chore: release (beta)

--- config/overrides/astro.overrides.js
+++ config/overrides/astro.overrides.js
@@ -20,5 +20,6 @@ module.exports = {
 		'astro/no-deprecated-getentrybyslug': 'error',
 		'astro/no-unused-define-vars-in-style': 'error',
 		'astro/valid-compile': 'error',
+		'prettier/prettier': 'off',
 	},
 };
