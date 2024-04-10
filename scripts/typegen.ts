8f09931bb397972a1143f180eb41934d072a1904
MarioMH8 <mhidalgomario@gmail.com>
2024-04-10 15:44:53 +0200

    feat: add typegen script

---
+++ scripts/typegen.ts
@@ -0,0 +1,21 @@
+import { flatConfigsToRulesDTS } from 'eslint-typegen/core'
+import { builtinRules } from 'eslint/use-at-your-own-risk'
+import { javascript } from '../src/config';
+import { combine } from '../src/utils';
+
+const configs = await combine(
+	{
+		plugins: {
+			'': {
+				rules: Object.fromEntries(builtinRules.entries()),
+			},
+		},
+	},
+	javascript()
+)
+
+const dts = await flatConfigsToRulesDTS(configs, {
+	includeAugmentation: false,
+})
+
+await Bun.write('src/typegen.d.ts', dts);
