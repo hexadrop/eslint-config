95aa2366743d77165b775b898b3e0205be3d6192
MarioMH8 <mhidalgomario@gmail.com>
2024-05-02 22:35:51 +0200

    fix: remove unused infer type

--- src/types/pick-rules.ts
+++ src/types/pick-rules.ts
@@ -1,5 +1,5 @@
 import type { TypedFlatConfigItem } from './typed-flat-config-item';
 
 export type PickRules<T extends TypedFlatConfigItem['rules'], S extends string> = {
-	[K in keyof T as K extends `${S}${infer _R}` ? K : never]: T[K]
+	[K in keyof T as K extends `${S}${string}` ? K : never]: T[K]
 }
