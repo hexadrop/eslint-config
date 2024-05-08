1895301cb04e46b0c68f8ba140fe30eaccc92b3c
MarioMH8 <mhidalgomario@gmail.com>
2024-05-08 02:42:19 +0200

    refactor: refactor options

--- src/config/ignore/index.ts
+++ src/config/ignore/index.ts
@@ -1,4 +1,3 @@
 export { default as ignore } from './ignore.config';
 export type * from './ignore.config-name';
 export { default as IGNORE_GLOB } from './ignore.globs';
-export type * from './ignore.options';
